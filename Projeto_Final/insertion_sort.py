9cc0496b0c7d625fa4087d0f0e035b155b526bcb
Ivo.araujo <86474505+ivoaraujo17@users.noreply.github.com>
2023-06-02 19:35:17 -0300

    algoritimo insertion_sort.py

--- Projeto_Final/insertion_sort.py
+++ Projeto_Final/insertion_sort.py
@@ -1,7 +1,6 @@
 def insertion_sort(array):
     tam = len(array)
     for pos_pivor in range(1, tam):
-<<<<<<< HEAD
         valor_atual = array[pos_pivor]
         j = pos_pivor
         # pecorre o array da posicao atual para tras até a posicao zero
@@ -12,12 +11,4 @@ def insertion_sort(array):
             j -= 1
         # inserindo o valor atual na posição correta
         array[j] = valor_atual
-=======
-        min = array[pos_pivor]
-        j = pos_pivor
-        while j > 0 and array[j-1] > min:
-            array[j] = array[j-1]
-            j -= 1
-        array[j] = min
->>>>>>> origin/master
     return array
